b378d5fc35c0f8f8be68e60d22c9f90a57deedb0
HoseinEshrati <eshrati@outlook.com>
2016-11-15 10:22:57 +0330

    finall project

--- Practic7/app/src/main/java/com/example/hosein/practic7/MainActivity.java
+++ Practic7/app/src/main/java/com/example/hosein/practic7/MainActivity.java
@@ -13,15 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button btsign=(Button)findViewById(R.id.signup);
-
+        final Button btlog=(Button)findViewById(R.id.login);
         btsign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent  = new Intent(MainActivity.this,Page2.class);
                 startActivity(intent);
+
+
+
             }
         });
 
+        btlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentlog = new Intent(MainActivity.this,Page3.class);
+                startActivity(intentlog);
+
+            }
+        });
     }
 }
--- Practic7/app/src/main/java/com/example/hosein/practic7/Page2.java
+++ Practic7/app/src/main/java/com/example/hosein/practic7/Page2.java
@@ -3,6 +3,7 @@ package com.example.hosein.practic7;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Page2 extends AppCompatActivity {
@@ -12,8 +13,14 @@ public class Page2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
-        Button btreg=(Button)findViewById(R.id.btreg);
+        Button btreg=(Button)findViewById(R.id.btregclick);
 
-
+btreg.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intentreg = new Intent(Page2.this,Page3.class);
+        startActivity(intentreg);
+    }
+});
     }
 }
