8fd5053eb432cb761eefbbc8d029517779b59e3c
HoseinEshrati <eshrati@outlook.com>
2016-11-15 17:30:27 +0330

    end practic 8 and strat practice 9

--- Practice8/app/src/main/java/com/example/hosein/practice8/MainActivity.java
+++ Practice8/app/src/main/java/com/example/hosein/practice8/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hosein.practice8;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.appname);
+
+
+        Button  noti =(Button)findViewById(R.id.notify);
+        Button  notno=(Button)findViewById(R.id.no);
+
+        noti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentnoti = new Intent(MainActivity.this,Page2.class);
+                startActivity(intentnoti);
+            }
+        });
+
+        notno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentno =new Intent(MainActivity.this,Page3.class);
+                startActivity(intentno);
+            }
+        });
+
 
     }
 }
